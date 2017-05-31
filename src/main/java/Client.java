cbc3ffbdcd7abe1344957755c0d2342b091b915f
sm880508@gmail.com <sm880508@gmail.com>
2017-05-31 11:23:43 +0800

    MQTT

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,11 +1,13 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.streaming.*;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-
+import scala.collection.mutable.ArrayBuffer;
+import org.apache.spark.streaming.mqtt.MQTTUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,32 +18,38 @@ import java.util.List;
  */
 public class Client {
     public static void main(String[] args) throws IOException,InterruptedException{
-
+        List<Integer> compressList = new ArrayList<Integer>();
+        String deString="";
         SparkConf sparkConf = new SparkConf().setMaster("local[*]").setAppName("receive");
         JavaStreamingContext javaStreamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(5));
         //JavaDStream<String> stream = javaStreamingContext.textFileStream("/home/yiwei/IdeaProjects/FPro/PubNub.txt").cache();
         JavaReceiverInputDStream<String> lines = javaStreamingContext.receiverStream(new MyReceiver("localhost",9999));
 
         JavaDStream<String> splitWord = lines.flatMap(s -> Arrays.asList(s.split(" ")).iterator());
-        JavaDStream<Integer> transferDec =splitWord.map(s -> Integer.parseInt(s,2));
+        JavaDStream<Integer> transferDec =splitWord.map(binary -> Integer.parseInt(binary,2));
+       transferDec.foreachRDD(new VoidFunction<JavaRDD<Integer>>() {
+           @Override
+           public void call(JavaRDD<Integer> rdd) throws Exception {
+               rdd.foreach(new VoidFunction<Integer>() {
+
+                   @Override
+                   public void call(Integer integer) throws Exception {
+                       compressList.add(integer);
 
 
+                   }
+               });
+           }
+
+       });
+
+        //transferDec.print();
+        deString = Decode.decompress(compressList);
+        System.out.println(deString);
         javaStreamingContext.start();
         javaStreamingContext.awaitTermination();
     }
-    private   List<Integer> toDec(List<String> list){
-        List<Integer> DecNum = new ArrayList<Integer>();
-        for (String s : list){
 
-            DecNum.add(Integer.parseInt(s,2));
-
-
-        }
-
-        System.out.println(DecNum);
-        return DecNum;
-
-    }
 
     //
 }
